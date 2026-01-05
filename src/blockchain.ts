9680d5ccabd7de09c56de90c850a4bc35c3a9966
Xavion-git <xdean1@nait.ca>
2026-01-05 01:51:59 -0700

    tsconfig, tslint JSON files added

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,4 +1,5 @@
 import * as CryptoJS from 'crypto-js';
+import {broadcastLatest} from './p2p';
 
 class Block {
 
@@ -24,8 +25,15 @@ const genesisBlock: Block = new Block(
     0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', '', 1465154705, 'my genesis block!!'
 );
 
+//storing the block in memory 
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
 // calculates the hash using the index, previousHash, timestamp, and the data 
-const calculateHash = (index: number, hash: string, previousHash: string, timestamp: number, data: string) =>
+const calculateHash = (index: number, previousHash: string, timestamp: number, data: string) =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
 const generateNextBlock = (blockData: string) => {
@@ -34,10 +42,18 @@ const generateNextBlock = (blockData: string) => {
     const nextTimestamp: number = new Date().getTime() / 1000;
     const nextHash: string = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);// Gets the new block 
     const newBlock: Block = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
+    addBlock(newBlock);
+    broadcastLatest();
     return newBlock;
 }
-//storing the block in memory 
-const blockchain: Block[] = [genesisBlock];
+const calculateHashForBlock = (block: Block): string =>
+    calculateHash(block.index, block.previousHash, block.timestamp, block.data);
+
+const addBlock = (newBlock: Block) => {
+    if (isValidNewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+    }
+};
 
 // Validation for integrity 
 const isValidNewBlock = (newBlock: Block, previousBlock: Block) => {
@@ -55,3 +71,49 @@ const isValidNewBlock = (newBlock: Block, previousBlock: Block) => {
     return true;
 }
 
+// validating the structure of the block for peer
+const isValidBlockStructure = (block: Block): boolean => {
+    return typeof block.index === 'number'
+        && typeof block.hash === 'string'
+        && typeof block.previousHash === 'string'
+        && typeof block.timestamp === 'number'
+        && typeof block.data === 'string';
+};
+
+// Validate the chain using the genesis Blockchain
+const isValidChain = (blockchainToValidate: Block[]): boolean => {
+    const isValidGenesis = (block: Block): boolean => {
+        return JSON.stringify(block) === JSON.stringify(genesisBlock);
+    };
+
+    if (!isValidGenesis(blockchainToValidate[0])) {
+        return false;
+    }
+
+    for (let i = 1; i < blockchainToValidate.length; i++) {
+        if (!isValidNewBlock(blockchainToValidate[i], blockchainToValidate[i - 1])) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const addBlockToChain = (newBlock: Block) => {
+    if (isValidNewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+        return true;
+    }
+    return false;
+};
+
+// Initial Conflicts so the longer chain dominates
+const replaceChain = (newBlocks: Block[]) => {
+    if (isValidChain(newBlocks) && newBlocks.length > getBlockchain().length) {
+        console.log('Received blockchain is valid. Replacing current blockchain with received blockchain');
+        blockchain = newBlocks;
+        broadcastLatest();
+    } else {
+        console.log('Received blockchain invalid');
+    }
+};
+export {Block, getBlockchain, getLatestBlock, generateNextBlock, isValidBlockStructure, replaceChain, addBlockToChain};
