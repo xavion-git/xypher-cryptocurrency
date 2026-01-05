fbf9940f9a80ef945a8d402e27ce9941ae6b8d13
Xavion-git <xdean1@nait.ca>
2026-01-05 00:41:21 -0700

    Adding blockchain

---
+++ src/blockchain.ts
@@ -0,0 +1,13 @@
+class Block {
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+
+    constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string){
+        this.index = index;
+    }
+
+}
