5a22db88df81d3ad5424ab0cb54d0d12391e4434
Xavion-git <xdean1@nait.ca>
2026-01-06 20:35:47 -0700

    getTransactionId, TxOut, TxIn

--- src/transaction.ts
+++ src/transaction.ts
@@ -20,3 +20,40 @@ class UnspentTxOut {
         this.amount = amount;
     }
 }
+
+class TxIn {
+    public txOutId: string;
+    public txOutIndex: number;
+    public signature: string;
+}
+
+class TxOut {
+    public address: string;
+    public amount: number;
+
+    constructor(address: string, amount: number){
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+class Transaction {
+
+
+    public id: string;
+
+    public txIns: TxIn[];
+    public txOuts: TxOut[];
+}
+
+const getTransactionId = (transaction: Transaction): string => {
+    const txInContent: string = transaction.txIns
+    .map((txIn: TxIn) => TxOut.address + TxOut.amount)
+    .reduce((a, b) => a + b, '');
+
+    const txOutContent: string = transaction.txOuts
+        .map((txOut: TxOut) => txOut.address + txOut.amount)
+        .reduce((a, b) => a + b, '');
+
+    return CryptoJS.SHA256(txInContent + txOutContent).toString();
+}
