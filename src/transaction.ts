53a1822ffcb6ce1c702453c6a7a7f14c6e266b90
Xavion-git <xdean1@nait.ca>
2026-01-07 10:21:12 -0700

    transaction err handling

--- src/transaction.ts
+++ src/transaction.ts
@@ -230,3 +230,117 @@ const updateUnspentTxOuts = (aTransactions: Transaction[], aUnspentTxOuts: Unspe
 
     return resultingUnspentTxOuts;
 };
+
+const processTransactions = (aTransactions: Transaction[], aUnspentTxOuts: UnspentTxOut[], blockIndex: number) => {
+
+    if (!isValidTransactionsStructure(aTransactions)) {
+        return null;
+    }
+
+    if (!validateBlockTransactions(aTransactions, aUnspentTxOuts, blockIndex)) {
+        console.log('invalid block transactions');
+        return null;
+    }
+    return updateUnspentTxOuts(aTransactions, aUnspentTxOuts);
+};
+
+const toHexString = (byteArray): string => {
+    return Array.from(byteArray, (byte: any) => {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+};
+
+const getPublicKey = (aPrivateKey: string): string => {
+    return ec.keyFromPrivate(aPrivateKey, 'hex').getPublic().encode('hex');
+};
+
+const isValidTxInStructure = (txIn: TxIn): boolean => {
+    if (txIn == null) {
+        console.log('txIn is null');
+        return false;
+    } else if (typeof txIn.signature !== 'string') {
+        console.log('invalid signature type in txIn');
+        return false;
+    } else if (typeof txIn.txOutId !== 'string') {
+        console.log('invalid txOutId type in txIn');
+        return false;
+    } else if (typeof  txIn.txOutIndex !== 'number') {
+        console.log('invalid txOutIndex type in txIn');
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const isValidTxOutStructure = (txOut: TxOut): boolean => {
+    if (txOut == null) {
+        console.log('txOut is null');
+        return false;
+    } else if (typeof txOut.address !== 'string') {
+        console.log('invalid address type in txOut');
+        return false;
+    } else if (!isValidAddress(txOut.address)) {
+        console.log('invalid TxOut address');
+        return false;
+    } else if (typeof txOut.amount !== 'number') {
+        console.log('invalid amount type in txOut');
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const isValidTransactionsStructure = (transactions: Transaction[]): boolean => {
+    return transactions
+        .map(isValidTransactionStructure)
+        .reduce((a, b) => (a && b), true);
+};
+
+const isValidTransactionStructure = (transaction: Transaction) => {
+    if (typeof transaction.id !== 'string') {
+        console.log('transactionId missing');
+        return false;
+    }
+    if (!(transaction.txIns instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+    if (!transaction.txIns
+            .map(isValidTxInStructure)
+            .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+
+    if (!(transaction.txOuts instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+
+    if (!transaction.txOuts
+            .map(isValidTxOutStructure)
+            .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+    return true;
+};
+
+//valid address is a valid ecdsa public key in the 04 + X-coordinate + Y-coordinate format
+const isValidAddress = (address: string): boolean => {
+    if (address.length !== 130) {
+        console.log('invalid public key length');
+        return false;
+    } else if (address.match('^[a-fA-F0-9]+$') === null) {
+        console.log('public key must contain only hex characters');
+        return false;
+    } else if (!address.startsWith('04')) {
+        console.log('public key must start with 04');
+        return false;
+    }
+    return true;
+};
+
+export {
+    processTransactions, signTxIn, getTransactionId,
+    UnspentTxOut, TxIn, TxOut, getCoinbaseTransaction, getPublicKey,
+    Transaction
+}
