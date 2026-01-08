30cf1ba1bef391db3669f92cc91b98d657b3e647
Xavion-git <xdean1@nait.ca>
2026-01-07 22:48:58 -0700

    Wallet.ts

---
+++ src/wallet.ts
@@ -0,0 +1,93 @@
+import {ec} from 'elliptic';
+import {existsSync, readFileSync, unlinkSync, writeFileSync} from 'fs';
+import _ from 'lodash';
+import {getPublicKey, getTransactionId, signTxIn, Transaction, TxIn, TxOut, UnspentTxOut} from './transaction';
+
+const EC = new ec('secp256k1');
+const privateKeyLocation = 'node/wallet/private_key';
+
+const getPrivateFromWallet = (): string => {
+    const buffer = readFileSync(privateKeyLocation, 'utf8');
+    return buffer.toString();
+};
+
+const getPublicFromWallet = (): string => {
+    const privateKey = getPrivateFromWallet();
+    const key = EC.keyFromPrivate(privateKey, 'hex');
+    return key.getPublic().encode('hex', false);
+};
+
+const generatePrivateKey = (): string => {
+    const keyPair = EC.genKeyPair();
+    const privateKey = keyPair.getPrivate();
+    return privateKey.toString(16);
+};
+
+const initWallet = () => {
+    // let's not override existing private keys
+    if (existsSync(privateKeyLocation)) {
+        return;
+    }
+    const newPrivateKey = generatePrivateKey();
+
+    writeFileSync(privateKeyLocation, newPrivateKey);
+    console.log('new wallet with private key created');
+};
+
+const getBalance = (address: string, unspentTxOuts: UnspentTxOut[]): number => {
+    return _(unspentTxOuts)
+        .filter((uTxO: UnspentTxOut) => uTxO.address === address)
+        .map((uTxO: UnspentTxOut) => uTxO.amount)
+        .sum();
+};
+
+const findTxOutsForAmount = (amount: number, myUnspentTxOuts: UnspentTxOut[]) => {
+    let currentAmount = 0;
+    const includedUnspentTxOuts = [];
+    for (const myUnspentTxOut of myUnspentTxOuts) {
+        includedUnspentTxOuts.push(myUnspentTxOut);
+        currentAmount = currentAmount + myUnspentTxOut.amount;
+        if (currentAmount >= amount) {
+            const leftOverAmount = currentAmount - amount;
+            return {includedUnspentTxOuts, leftOverAmount};
+        }
+    }
+    throw Error('not enough coins to send transaction');
+};
+
+const createTxOuts = (receiverAddress: string, myAddress: string, amount: number, leftOverAmount: number) => {
+    const txOut1: TxOut = new TxOut(receiverAddress, amount);
+    if (leftOverAmount === 0) {
+        return [txOut1];
+    } else {
+        const leftOverTx = new TxOut(myAddress, leftOverAmount);
+        return [txOut1, leftOverTx];
+    }
+};
+
+const createTransaction = (receiverAddress: string, amount: number,
+                           privateKey: string, unspentTxOuts: UnspentTxOut[]): Transaction => {
+
+    const myAddress: string = getPublicKey(privateKey);
+    const myUnspentTxOuts = unspentTxOuts.filter(uTxO => uTxO.address === myAddress);
+
+    const { includedUnspentTxOuts, leftOverAmount } = findTxOutsForAmount(amount, myUnspentTxOuts);
+
+    const unsignedTxIns: TxIn[] = includedUnspentTxOuts.map(uTxO => new TxIn(uTxO.txOutId, uTxO.txOutIndex, ''));
+
+    const tx: Transaction = new Transaction(unsignedTxIns, createTxOuts(receiverAddress, myAddress, amount, leftOverAmount));
+
+    // Set transaction ID
+    tx.id = getTransactionId(tx);
+
+    // Sign each txIn
+    tx.txIns = tx.txIns.map((txIn: TxIn, index: number) => {
+        txIn.signature = signTxIn(tx, index, privateKey, unspentTxOuts);
+        return txIn;
+    });
+
+    return tx;
+};
+
+export {createTransaction, getPublicFromWallet,
+    getPrivateFromWallet, getBalance, generatePrivateKey, initWallet};
