d5b6f5133a693fc18bb0e718b30ff144186a994d
Xavion-git <xdean1@nait.ca>
2026-02-08 13:50:55 -0700

    Dashboard HTML

--- xypher-ui/src/app/features/dashboard/dashboard.component.ts
+++ xypher-ui/src/app/features/dashboard/dashboard.component.ts
@@ -31,10 +31,29 @@ export class DashboardComponent implements OnInit {
     this.address$ = this.walletService.address$;
     this.transactionPool$ = this.blockchainService.transactionPool$;
   }
-  
 
+  ngOnInit(): void {
+    this.loadData();
+    
+    // Subscribe to blocks for stats
+    this.blocks$.subscribe(blocks => {
+      this.latestBlocks = blocks.slice(-5).reverse();
+      this.networkStats.totalBlocks = blocks.length;
+      this.networkStats.difficulty = blocks[blocks.length - 1]?.difficulty || 0;
+    });
+    
+    this.transactionPool$.subscribe(pool => {
+      this.networkStats.pendingTransactions = pool.length;
+    });
+  }
 
+  loadData(): void {
+    this.blockchainService.loadBlockchain();
+    this.blockchainService.loadTransactionPool();
+    this.walletService.loadWalletData();
+  }
 
-
-
+  refresh(): void {
+    this.loadData();
+  }
 }
