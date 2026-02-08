a0bb58fee0f3c36b65abfff906e3ecce37c091ca
Xavion-git <xdean1@nait.ca>
2026-02-08 13:34:53 -0700

    Getting blockchain.service
    Creates a read-only Observable from the BehaviorSubject

--- xypher-ui/src/app/core/services/blockchain.service.ts
+++ xypher-ui/src/app/core/services/blockchain.service.ts
@@ -24,5 +24,75 @@ export class BlockchainService {
     }
 
     // Get Blockchain 
-    
+    getBlockchain(): Observable<Block[]> {
+        return this.http.get<Block[]>(`${this.apiUrl}/blocks`).pipe(
+            tap(blocks => this. blocksSubject.next(blocks))
+        );
+    }
+
+    loadBlockchain(): void {
+    this.getBlockchain().subscribe();
+  }
+
+  // Get specific block
+  getBlock(hash: string): Observable<Block> {
+    return this.http.get<Block>(`${this.apiUrl}/block/${hash}`);
+  }
+
+  // Get transaction
+  getTransaction(id: string): Observable<Transaction> {
+    return this.http.get<Transaction>(`${this.apiUrl}/transaction/${id}`);
+  }
+
+  // Get unspent outputs
+  getUnspentTxOuts(): Observable<UnspentTxOut[]> {
+    return this.http.get<UnspentTxOut[]>(`${this.apiUrl}/unspentTransactionOutputs`);
+  }
+
+  // Get unspent outputs for address
+  getAddressUnspentTxOuts(address: string): Observable<{unspentTxOuts: UnspentTxOut[]}> {
+    return this.http.get<{unspentTxOuts: UnspentTxOut[]}>(`${this.apiUrl}/address/${address}`);
+  }
+
+  // Mine new block
+  mineBlock(): Observable<Block> {
+    return this.http.post<Block>(`${this.apiUrl}/mineBlock`, {}).pipe(
+      tap(() => this.loadBlockchain())
+    );
+  }
+
+  // Mine block with transaction
+  mineTransaction(address: string, amount: number): Observable<Block> {
+    return this.http.post<Block>(`${this.apiUrl}/mineTransaction`, { address, amount }).pipe(
+      tap(() => this.loadBlockchain())
+    );
+  }
+
+  // Send transaction to pool
+  sendTransaction(address: string, amount: number): Observable<Transaction> {
+    return this.http.post<Transaction>(`${this.apiUrl}/sendTransaction`, { address, amount }).pipe(
+      tap(() => this.loadTransactionPool())
+    );
+  }
+
+  // Get transaction pool
+  getTransactionPool(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(`${this.apiUrl}/transactionPool`).pipe(
+      tap(pool => this.transactionPoolSubject.next(pool))
+    );
+  }
+
+  loadTransactionPool(): void {
+    this.getTransactionPool().subscribe();
+  }
+
+  // Get peers
+  getPeers(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/peers`);
+  }
+
+  // Add peer
+  addPeer(peer: string): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/addPeer`, { peer });
+  }
 }
