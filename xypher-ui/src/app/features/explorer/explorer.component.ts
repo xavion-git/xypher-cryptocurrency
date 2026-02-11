5873ee4ae3a5711d8a149785d8a25bb728712b15
Xavion-git <xdean1@nait.ca>
2026-02-10 22:45:34 -0700

    Explorer Component with styling

---
+++ xypher-ui/src/app/features/explorer/explorer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explorer',
+  templateUrl: './explorer.component.html',
+  styleUrls: ['./explorer.component.scss']
+})
+export class ExplorerComponent {
+}
